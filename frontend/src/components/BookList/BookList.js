8d558094588530a25629c750b27524e214c641f1
ilya <ilyakhlamov77@gmail.com>
2024-08-14 17:03:36 +0300

    add deleteBook

--- frontend/src/components/BookList/BookList.js
+++ frontend/src/components/BookList/BookList.js
@@ -1,9 +1,15 @@
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { deleteBook } from '../../redux/books/actionCreators'
 
 import './BookList.css'
 
 const BookList = () => {
 	const books = useSelector(state => state.books)
+    const dispatch = useDispatch()
+
+    const handleDeleteBook = (id) => {
+        dispatch(deleteBook(id))
+    }
 
 	return (
 		<div className='app-block book-list'>
@@ -17,6 +23,10 @@ const BookList = () => {
 							<div className='book-info'>
 								{++i}. {book.title} by <strong>{book.author}</strong>
 							</div>
+                            <div className='book-actions'>
+								<button onClick={() => handleDeleteBook(book.id)}>Delete</button>
+
+                            </div>
 						</li>
 					))}
 				</ul>
--- frontend/src/redux/books/actionCreators.js
+++ frontend/src/redux/books/actionCreators.js
@@ -1,8 +1,15 @@
 import * as actionTypes from './actionTypes'
 
-export const addBook = (newBook) => {
-    return {
-        type: actionTypes.ADD_BOOK,
-        payload: newBook
-    }
+export const addBook = newBook => {
+	return {
+		type: actionTypes.ADD_BOOK,
+		payload: newBook,
+	}
+}
+
+export const deleteBook = id => {
+	return {
+		type: actionTypes.DELETE_BOOK,
+		payload: id,
+	}
 }
