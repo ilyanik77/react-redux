087d5b145373a24978018bc73e57aa2236ca417b
ilya <ilyakhlamov77@gmail.com>
2024-08-15 11:38:25 +0300

    add filter by title

--- frontend/src/components/Filter/Filter.js
+++ frontend/src/components/Filter/Filter.js
@@ -1,11 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux'
+import {
+	setTitleFilter,
+	selectTitleFilter,
+} from '../../redux/slices/filterSlice'
+
 import './Filter.css'
 
 const Filter = () => {
-  return (
-    <div className='app-block filter'>
-        <h2>Filters</h2>
-    </div>
-  )
+	const dispatch = useDispatch()
+    const titleFilter = useSelector(selectTitleFilter)
+
+	const handleTitleFilterChange = e => {
+		dispatch(setTitleFilter(e.target.value))
+	}
+
+	return (
+		<div className='app-block filter'>
+			<div className='filter-group'>
+				<input
+					type='text'
+					placeholder='Filter by title...'
+                    value={titleFilter}
+					onChange={handleTitleFilterChange}
+				/>
+			</div>
+		</div>
+	)
 }
 
 export default Filter
