1af5fc7f860775e7cb7a19c141f993f002689bed
ilya <ilyakhlamov77@gmail.com>
2024-08-17 21:07:02 +0300

    Dispatch error in the async thunk

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,13 +1,23 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 import createBookWithId from '../../utils/createBookWithId'
+import { setError } from './errorSlice'
 
 const initialState = []
 
-export const fetchBook = createAsyncThunk('books/fetchBook', async () => {
-	const res = await axios.get('http://localhost:4000/random-book')
-	return res.data
-})
+export const fetchBook = createAsyncThunk(
+	'books/fetchBook',
+	async (url, thunkAPI) => {
+		try {
+			const res = await axios.get(url)
+			return res.data
+		} catch (error) {
+			thunkAPI.dispatch(setError(error.message))
+            throw error
+            
+		}
+	}
+)
 
 const booksSlice = createSlice({
 	name: 'books',
@@ -27,13 +37,13 @@ const booksSlice = createSlice({
 			})
 		},
 	},
-    extraReducers: (builder) => {
-        builder.addCase(fetchBook.fulfilled, (state, action) => {
-            if(action.payload.title && action.payload.author) {
-                state.push(createBookWithId(action.payload, 'API'))
-            }
-        })
-    }
+	extraReducers: builder => {
+		builder.addCase(fetchBook.fulfilled, (state, action) => {
+			if (action.payload.title && action.payload.author) {
+				state.push(createBookWithId(action.payload, 'API'))
+			}
+		})
+	},
 })
 
 export default booksSlice.reducer
