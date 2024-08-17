3cf8eda0029e7ae6b6663151e628950dc07ce82a
ilya <ilyakhlamov77@gmail.com>
2024-08-17 18:01:28 +0300

    add createAsyncThunk

--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -1,44 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit"
-import axios from "axios"
-import createBookWithId from "../../utils/createBookWithId"
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+import createBookWithId from '../../utils/createBookWithId'
 
 const initialState = []
 
-const booksSlice = createSlice({
-    name: 'books',
-    initialState,
-    reducers: {
-        addBook: (state, action) => {
-            state.push(action.payload)
-        },
-        deleteBook: (state, action) => {
-            return state.filter((book) => book.id !== action.payload)
-        }, 
-        toggleFavorite: (state, action) => {
-            state.forEach((book) => {
-                if (book.id === action.payload) {
-                    book.isFavorite = !book.isFavorite
-                }
-            })
-        }
+export const fetchBook = createAsyncThunk('books/fetchBook', async () => {
+	const res = await axios.get('http://localhost:4000/random-book')
+	return res.data
+})
 
+const booksSlice = createSlice({
+	name: 'books',
+	initialState,
+	reducers: {
+		addBook: (state, action) => {
+			state.push(action.payload)
+		},
+		deleteBook: (state, action) => {
+			return state.filter(book => book.id !== action.payload)
+		},
+		toggleFavorite: (state, action) => {
+			state.forEach(book => {
+				if (book.id === action.payload) {
+					book.isFavorite = !book.isFavorite
+				}
+			})
+		},
+	},
+    extraReducers: (builder) => {
+        builder.addCase(fetchBook.fulfilled, (state, action) => {
+            if(action.payload.title && action.payload.author) {
+                state.push(createBookWithId(action.payload, 'API'))
+            }
+        })
     }
 })
 
-
-export  const thunkFunction = async (dispatch, getState) => {
-    try {
-		const res = await axios.get('http://localhost:4000/random-book')
-		if (res?.data?.title && res?.data?.author) {
-            dispatch(addBook(createBookWithId(res.data, 'API')))
-		}
-	} catch (error) {
-		console.log('Error', error)
-	}
-}
-
 export default booksSlice.reducer
 
-export const selectBooks = (state) => state.books
+export const selectBooks = state => state.books
 
-export const {addBook, deleteBook, toggleFavorite} = booksSlice.actions
+export const { addBook, deleteBook, toggleFavorite } = booksSlice.actions
